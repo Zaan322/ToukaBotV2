12a50b347b4b0c492ade3d823ecab1bfefbc0b4a
Irzan <79050885+Zaan322@users.noreply.github.com>
2021-03-17 14:00:50 +0800

    Add files via upload

---
+++ ZanGanz/donasi.js
@@ -0,0 +1,18 @@
+const donasi = () => { 
+	return `       
+┏━━━━━━━━━━━━━━━━━━━━
+┃        *_DONASI_*  
+┣━━━━━━━━━━━━━━━━━━━━
+┣━⊱❉ DONASI BOSS ❉⊰━━✿
+┃  
+┣━⊱ *PULSA*
+┣⊱ 0853-4082-7717
+┃
+┣━⊱ *DANA*
+┣⊱ 0853-4082-7717
+┃
+┃Donasi Biar Fitur nya ga error
+┗━━━━━━━━━━━━━━━━━━━━`
+}
+
+exports.donasi = donasi
---
+++ ZanGanz/sewabot.js
@@ -0,0 +1,18 @@
+const sewabot = () => { 
+	return `
+Sewa BOT
+
+10k = ganti owner, ganti nomer owner, ganti nama bot, no apikey
+25k = ganti owner, ganti nomer owner, ganti nama bot, ada apikey
+SELAMA 3 HARI
+
+20k = ganti owner, ganti nomer owner, ganti nama bot, no apikey
+50k = ganti owner, ganti nomer owner, ganti nama bot, ada apikey
+SELAMA 1 MINGGU
+
+minat? hubungi wa.me/6282291992581
+┃
+pembayaran via Pulsa/Dana
+`
+}
+exports.sewabot = sewabot
